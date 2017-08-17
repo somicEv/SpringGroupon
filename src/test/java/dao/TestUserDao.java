7673bb796d44b7de3efc9b6e0f6991cfe6a23ca8
yuhaohan <yuhaohan@btte.net>
2017-08-17 22:49:16 +0800

    初步完成用户登录注册功能

--- src/test/java/dao/TestUserDao.java
+++ src/test/java/dao/TestUserDao.java
@@ -1,8 +1,10 @@
 package dao;
 
 import com.common.entity.user.User;
+import com.common.entity.user.UserBasicInfo;
 import com.database.UserBasicInfoDao;
 import com.database.UserDao;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,29 @@ public class TestUserDao {
 	@Autowired
 	private UserBasicInfoDao userBasicInfoDao;
 
+
+	public User createUser(){
+		User user = new User();
+		user.setName("测试");
+		user.setPassword("123456");
+		user.setLoginTime(new Date());
+		user.setCreateTime(new Date());
+		user.setUpdateTime(new Date());
+		return user;
+	}
+
+
+	public UserBasicInfo createInfo(){
+		UserBasicInfo userBasicInfo = new UserBasicInfo();
+		userBasicInfo.setNickname("asdf");
+		userBasicInfo.setRealName("qwer");
+		userBasicInfo.setPhone("1");
+		userBasicInfo.setMail("13011182178@163.com");
+		userBasicInfo.setCreateTime(new Date());
+		userBasicInfo.setUpdateTime(new Date());
+		return userBasicInfo;
+	}
+
 	@Test
 	public void testGetAll(){
 		User user = new User();
@@ -29,18 +54,11 @@ public class TestUserDao {
 		for (User u : userList){
 			System.out.println(u);
 		}
-
-		UserIn
 	}
 
 	@Test
 	public void testInsert(){
-		User user = new User();
-		user.setName("测试");
-		user.setPassword("123456");
-		user.setLoginTime(new Date());
-		user.setCreateTime(new Date());
-		user.setUpdateTime(new Date());
+		User user = createUser();
 		Integer result = userDao.add(user);
 		System.out.println("插入结果---"+result);
 	}
@@ -60,6 +78,35 @@ public class TestUserDao {
 		user.setId(4);
 		Integer delete = userDao.delete(user);
 		System.out.println("更新的行数："+delete);
+	}
+
+	@Test
+	public void testUserBasisInfoAdd(){
+		UserBasicInfo info = createInfo();
+		Integer add = userBasicInfoDao.add(info);
+		System.out.println(add);
+	}
+
+	@Test
+	public void testUserBasicInfoUpdate(){
+		UserBasicInfo info = createInfo();
+		info.setId(2);
+		info.setRealName("qwer");
+		Integer update = userBasicInfoDao.update(info);
+		System.out.println(update);
+	}
+
+	@Test
+	public void testUserBasicInfoGet(){
+		UserBasicInfo userBasicInfo = new UserBasicInfo();
+		List<UserBasicInfo> userBasicInfos = userBasicInfoDao.get(userBasicInfo);
+		System.out.println(userBasicInfos);
+	}
+	@Test
+	public void testUserBasicInfoDelete(){
+		UserBasicInfo userBasicInfo = new UserBasicInfo();
+		userBasicInfo.setId(2);
+		userBasicInfoDao.delete(userBasicInfo);
 	}
 
 }
